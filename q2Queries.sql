51b67cdd3e9cdb03050a5d4be3e19427d66799d3
MitchellSchijen <m.r.m.j.schijen@student.tue.nl>
2019-02-21 16:09:05 +0100

    Added part 2 - query 1

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,4 +1,7 @@
--- Q1 Mitchell
+-- Q1 Mitchell 
+-- valid StudentId and DegreeId in newer dataset
+-- else use SELECT DegreeId FROM StudentRegistrationsToDegrees WHERE (StudentId = 1); to find a suitable subject
+SELECT Courses.CourseName, CourseRegistrations.Grade FROM Students INNER JOIN StudentRegistrationsToDegrees on (Students.StudentId = StudentRegistrationsToDegrees.StudentId) INNER JOIN CourseRegistrations on (StudentRegistrationsToDegrees.StudentRegistrationId = CourseRegistrations.StudentRegistrationId) INNER JOIN CourseOffers on (CourseRegistrations.CourseOfferId = CourseOffers.CourseOfferId) INNER JOIN Courses on (CourseOffers.CourseId = Courses.CourseId) WHERE (Students.StudentId = 2 AND StudentRegistrationsToDegrees.DegreeId = 1099);
 
 -- Q2 Mitchell
 
