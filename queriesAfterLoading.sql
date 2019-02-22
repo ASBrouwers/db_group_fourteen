6eca9a24175c7f5410f94f4201f52a34aea41be7
FabiennevdWeide <f.p.v.d.weide@student.tue.nl>
2019-02-22 12:04:23 +0100

    Comments query numbers

--- queriesAfterLoading.sql
+++ queriesAfterLoading.sql
@@ -1,3 +1,4 @@
+-- Query I
 SELECT (CourseOffers.CourseOfferId, CourseOffers.CourseId, CourseOffers.Year, CourseOffers.Quartile, Courses.CourseName, Courses.CourseDescription, Courses.DegreeId, Courses.ECTS, Degrees.Dept, Degrees.DegreeDescription, Degrees.TotalECTS, Teachers.TeacherId, Teachers.TeacherName, Teachers.Address, Teachers.BirthyearTeacher, Teachers.Gender) FROM CourseOffers
   INNER JOIN Courses on (CourseOffers.CourseId = Courses.CourseId)
   INNER JOIN Degrees on (Courses.DegreeId = Degrees.DegreeId)
@@ -6,4 +7,8 @@ SELECT (CourseOffers.CourseOfferId, CourseOffers.CourseId, CourseOffers.Year, Co
   INNER JOIN Teachers on (TeacherAssignmentsToCourses.TeacherId = Teachers.TeacherId)
   WHERE CourseOffers.CourseOfferId = '1';
 
+-- Query II
+
+
+-- Query III
 SELECT AVG(Grade) FROM  CourseRegistrations INNER JOIN StudentRegistrationsToDegrees on (StudentRegistrationsToDegrees.StudentRegistrationId = CourseRegistrations.StudentRegistrationId) WHERE StudentRegistrationsToDegrees.StudentRegistrationId = 140;
