56643de4bacfcb74d91c613f821c55671c482f42
MitchellSchijen <m.r.m.j.schijen@student.tue.nl>
2019-03-08 15:30:19 +0100

    re-added pk and index on courseregistrations, and new view of q2 excellent students

--- q2Create.sql
+++ q2Create.sql
@@ -1,3 +1,5 @@
+ALTER TABLE CourseRegistrations ADD PRIMARY KEY (CourseOfferId, StudentRegistrationId);
+CREATE INDEX idx_courseRegs ON CourseRegistrations(CourseOfferId);
 CREATE MATERIALIZED VIEW MaxGrades AS SELECT CourseRegistrations.CourseOfferId, MAX(Grade) AS MaxGrade FROM CourseRegistrations JOIN CourseOffers ON (CourseOffers.CourseOfferId = CourseRegistrations.CourseOfferId) WHERE Year = 2018 AND Quartile = 1 GROUP BY CourseRegistrations.CourseOfferId;
 CREATE MATERIALIZED VIEW GPAAndECTSCount AS SELECT CourseRegistrations.StudentRegistrationId, SUM(CourseRegistrations.Grade * Courses.ECTS) / CAST(SUM(Courses.ECTS) AS float) AS GPA, SUM(Courses.ECTS) AS TotalECTSAcquired FROM CourseRegistrations INNER JOIN CourseOffers on (CourseRegistrations.CourseOfferId = CourseOffers.CourseOfferId) INNER JOIN Courses on (CourseOffers.COurseId = Courses.CourseId) WHERE (CourseRegistrations.Grade >= 5.0) GROUP BY CourseRegistrations.StudentRegistrationId;
 CREATE MATERIALIZED VIEW ActiveStudents AS 
@@ -12,3 +14,4 @@ CREATE MATERIALIZED VIEW NrOfAssistants AS
 SELECT CourseOffers.CourseOfferId, COUNT(StudentAssistants.StudentRegistrationId) AS NrOfAssistants
 FROM CourseOffers INNER JOIN StudentAssistants on (CourseOffers.CourseOfferId = StudentAssistants.CourseOfferId)
 GROUP BY CourseOffers.CourseOfferId;
+CREATE MATERIALIZED VIEW ExcellentStudentId AS SELECT StudentRegistrationsToDegrees.StudentId FROM StudentRegistrationsToDegrees INNER JOIN CourseRegistrations on (StudentRegistrationsToDegrees.StudentRegistrationId = CourseRegistrations.StudentRegistrationId) Group By StudentRegistrationsToDegrees.StudentId HAVING (COUNT(CASE WHEN CourseRegistrations.Grade < 5.0 THEN 1 WHEN CourseRegistrations.Grade IS NULL THEN 1 END) = 0) ORDER BY StudentRegistrationsToDegrees.StudentId;
