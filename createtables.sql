692a691038b3968555346786e3dade5b1b3c4132
ASBrouwers <a.s.brouwers@student.tue.nl>
2019-03-11 09:59:50 +0100

    removed empty space/comments

--- createtables.sql
+++ createtables.sql
@@ -1,4 +1,3 @@
--- Create Students table
 CREATE UNLOGGED TABLE Degrees(DegreeId int PRIMARY KEY, Dept varchar(50), DegreeDescription varchar(200), TotalECTS smallint);
 CREATE UNLOGGED TABLE Students(StudentId int PRIMARY KEY, StudentName varchar(50), Address varchar(200),BirthyearStudent smallint, Gender char);
 CREATE UNLOGGED TABLE StudentRegistrationsToDegrees(StudentRegistrationId int PRIMARY KEY, StudentId int, DegreeId int, RegistrationYear smallint);
@@ -7,5 +6,4 @@ CREATE UNLOGGED TABLE Courses(CourseId int PRIMARY KEY, CourseName varchar(50), 
 CREATE UNLOGGED TABLE CourseOffers(CourseOfferId int PRIMARY KEY, CourseId int, Year smallint, Quartile smallint);
 CREATE UNLOGGED TABLE TeacherAssignmentsToCourses(CourseOfferId int, TeacherId int);
 CREATE UNLOGGED TABLE StudentAssistants(CourseOfferId int, StudentRegistrationId int, PRIMARY KEY(CourseOfferId,StudentRegistrationId));
--- could add PK to courseregistrations here, but this leads to a massive (+40 min) increase in execution time of importing data
 CREATE UNLOGGED TABLE CourseRegistrations(CourseOfferId int, StudentRegistrationId int, Grade smallint);
--- load.sh
+++ load.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#load script group 14
 psql -d uni -f createtables.sql
 psql -d uni -f loaddata.sql
 
